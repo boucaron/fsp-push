73951af56ac684bab14fc5cb95bf71e63fa36ae6
Julien BOUCARON <julien.boucaron@gmail.com>
2026-03-08 16:37:30 +0100

    Add datastore dep

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 
     // SSH client
     implementation(libs.jsch)
